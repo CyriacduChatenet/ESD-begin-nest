6aa555376c76278cf120bf092b35896fe24dc1e9
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-01 09:21:34 +0100

    [FEAT]: init front

---
+++ client/src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+	const [count, setCount] = useState(0);
+	const [data, setData] = useState([]);
+
+	const handleFetch = async () => {
+		const response = await fetch('http://localhost:8000/api/v1/post');
+		const data = await response.json();
+		setData(data);
+	};
+
+	useEffect(() => {
+		handleFetch();
+	}, []);
+
+	return (
+		<div className="App">
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://reactjs.org" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			{data.map((item: any) => (
+				<div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.content}</p>
+          <p>{item.createdAt}</p>
+        </div>
+			))}
+		</div>
+	);
+}
+
+export default App;
